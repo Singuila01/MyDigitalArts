4963a01771379316fb8364dac497291ce4502423
Singuila01 <mbayechris201@gmail.com>
2025-04-17 13:41:54 +0200

    Modif de web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CardCategoryController;
 use App\Http\Controllers\CardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,11 +13,20 @@ Route::group(['prefix'=>'cards'], function(){
     Route::get('/index', [CardController::class, 'index'])->name('cards.index');
 });
 
-Route::get('/categories', [CardCategoryController::class, "index"]);
+// Route::get('/categories', [CardCategoryController::class, "index"]);
+Route::get('/categories', function () {
+    return view('categories.index');
+});
+
+Route::get('/cards', [CardController::class, "index"]);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/about', function () { return 'À propos'; }); // Exemple temporaire
+// Route::get('/about', function () { return 'À propos'; }); // Exemple temporaire
+// Route::get('/about', [UserController::class, 'index'])->name(name: 'auth.about'); // Exemple temporaire
+Route::get('/user', function () {
+    return view('auth.user');
+});
