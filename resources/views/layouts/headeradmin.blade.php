5d667f52c13ec53e2c72cd509465a16fe5d315e2
Singuila01 <mbayechris201@gmail.com>
2025-05-11 13:22:09 +0200

    Modification de l'en-tête pour l'admin

--- resources/views/layouts/headeradmin.blade.php
+++ resources/views/layouts/headeradmin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyDigitalArt</title>
+    <title>@yield('title', 'Liste des cartes')</title>
     <!-- CDN Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
